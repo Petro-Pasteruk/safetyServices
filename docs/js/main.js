0495ab1ee4ab8027785acf4734b2f1c8a2e80d81
petro_pasteruk <petropasteruk@gmail.com>
2021-04-27 15:59:42 +0300

    last version

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,26 +1,16 @@
-$(document).ready(function(){
-
-    // Init Sliders
+$(document).ready(function () {
     const optionSlider = {
         slidesPerView: 1,
         spaceBetween: 30,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
+        navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
         breakpoints: {
-          600: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          992: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          },
-        },
-      };
-
-    // Init sliders, add event switching slides
+            600: { slidesPerView: 1, centeredSlides: true },
+            700: {slidesPerView: 2, spaceBetween: 20 },
+            992: { slidesPerView: 2, spaceBetween: 40 },
+            1450: {slidesPerView: 3},
+            2480: {slidesPerView: 4},
+            3650: {slidesPerView: 5}},
+    };
     const
         allSlidersWrap = document.querySelectorAll(".services__slider"),
         slidersInit = [];
@@ -30,31 +20,26 @@ $(document).ready(function(){
     });
 
     allSlidersWrap.forEach((item, index) => {
-        slidersInit.push(new Swiper (".services__slider[data-id='" + index + "']", optionSlider));
-
+        slidersInit.push(new Swiper(".services__slider[data-id='" + index + "']", optionSlider));
         item.parentElement.querySelector(".prevBtn").addEventListener("click", function () {
             slidersInit[index].slidePrev();
         });
-
         item.parentElement.querySelector(".nextBtn").addEventListener("click", function () {
             slidersInit[index].slideNext();
         });
     });
 
-    // Hide sliders
     const allSliders = document.querySelectorAll(".services__slider-wrap");
-
     allSliders.forEach((item, index) => {
         if (index !== 0) {
             item.classList.add("hidden");
         }
     });
-
-    // Toggle Services tab
+    document.querySelector(".services__item").classList.add("active");
     const allServicesBtns = document.querySelectorAll(".services__item");
     allServicesBtns.forEach((item, index) => {
         item.addEventListener("click", function (e) {
-            allServicesBtns.forEach(item => {
+            allServicesBtns.forEach((item) => {
                 item.classList.remove("active");
             });
             item.classList.add("active");
@@ -67,10 +52,8 @@ $(document).ready(function(){
             });
         });
     });
-
-    // Open main menu
-    const $sidesMenuBtn = $('.sides-menu__btn');
-    $sidesMenuBtn.on('click', function(e) {
+    const $sidesMenuBtn = $(".sides-menu__btn");
+    $sidesMenuBtn.on("click", function (e) {
         if ($(this).hasClass("close")) {
             $sidesMenuBtn.removeClass("close");
             $(".sides-menu__btn-nav").removeClass("close");
@@ -83,10 +66,8 @@ $(document).ready(function(){
             $("body").css("overflow", "hidden");
         }
     });
-
-    // Click main menu link
     const allNavItem = document.querySelectorAll(".nav__item");
-    allNavItem.forEach(item => {
+    allNavItem.forEach((item) => {
         item.addEventListener("click", function () {
             $(".sides-menu__btn").removeClass("close");
             $(".sides-menu__btn-nav").removeClass("close");
@@ -94,102 +75,48 @@ $(document).ready(function(){
             $("body").css("overflow", "inherit");
         });
     });
-
-    // Smooth scrolling
-    $("#menu").on("click","a", function (e) {
+    $("#menu").on("click", "a", function (e) {
         e.preventDefault();
-        const
-            id  = $(this).attr('href'),
+        const id = $(this).attr("href"),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
+        $("body,html").animate({ scrollTop: top }, 1500);
     });
-
-    // Validation form
-    const
-        nameReg = /(^[A-Z]{1}[a-z]{1,14} [A-Z]{1}[a-z]{1,14}$)|(^[А-Я]{1}[а-я]{1,14} [А-Я]{1}[а-я]{1,14}$)/,
+    const nameReg = /(^[A-Z]{1}[a-z]{1,14} [A-Z]{1}[a-z]{1,14}$)|(^[А-Я]{1}[а-я]{1,14} [А-Я]{1}[а-я]{1,14}$)/,
         emailReg = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/,
         phoneReg = /^((\+)?[1-9]{1,2})?([-\s\.])?((\(\d{1,4}\))|\d{1,4})(([-\s\.])?[0-9]{1,12}){1,2}$/;
-
-    function inputValidation ($input, regular) {
+    function inputValidation($input, regular) {
         $input.on("input", function (e) {
             const $submitBtn = $(".questions__btn");
-
             if (regular.test($input.val())) {
                 $input.css("border-color", "#1071b8");
                 e.target.parentElement.parentElement.querySelector(".error-text").classList.remove("active");
             } else {
                 $input.css("border-color", "red");
                 e.target.parentElement.parentElement.querySelector(".error-text").classList.add("active");
-                $submitBtn.disabled = true;
+                $submitBtn.disabled = !0;
                 $submitBtn.addClass("disabled");
             }
-
             if (nameReg.test($("#name")) && emailReg.test($("#email")) && phoneReg.test($("#phone"))) {
-                $submitBtn.disabled = false;
+                $submitBtn.disabled = !1;
                 $submitBtn.removeClass("disabled");
             }
         });
     }
-
     inputValidation($("#name"), nameReg);
     inputValidation($("#email"), emailReg);
     inputValidation($("#phone"), phoneReg);
-
     $("#text").on("input", function (e) {
         const $submitBtn = $(".questions__btn");
-
-       if ($("#text").val().trim().length > 5) {
-           $("#text").css("border-color", "#1071b8");
-           e.target.parentElement.parentElement.querySelector(".error-text").classList.remove("active");
-           $submitBtn.disabled = false;
-           $submitBtn.removeClass("disabled");
-       } else {
-           $("#text").css("border-color", "red");
-           e.target.parentElement.parentElement.querySelector(".error-text").classList.add("active");
-           $submitBtn.disabled = true;
-           $submitBtn.addClass("disabled");
-       }
-    });
-
-
-    // Adjust the slider to display correctly when adaptive
-    function searchGreatestHeight (arrayBlocks) {
-        let result = 0;
-
-        arrayBlocks.forEach(item => {
-            if (item.offsetHeight > result) {
-                result = item.offsetHeight;
-            }
-        });
-
-        return result;
-    }
-    function setHeight (arrayBlocks) {
-        let height = searchGreatestHeight(arrayBlocks);
-
-        arrayBlocks.forEach(item => {
-            item.style.height = height + "px";
-        });
-    }
-    function resetHeight (arrayBlocks) {
-        arrayBlocks.forEach(item => {
-            item.style.height = "initial";
-        });
-    }
-
-    const
-        allSwiperSlide = document.querySelectorAll(".swiper-slide"),
-        allImgBlockSlide = document.querySelectorAll(".slide__img-block");
-
-    setHeight(allSwiperSlide);
-    setHeight(allImgBlockSlide);
-
-    window.addEventListener(`resize`, () => {
-
-        resetHeight(allSwiperSlide);
-        resetHeight(allImgBlockSlide);
-
-        setHeight(allSwiperSlide);
-        setHeight(allImgBlockSlide);
+        if ($("#text").val().trim().length > 5) {
+            $("#text").css("border-color", "#1071b8");
+            e.target.parentElement.parentElement.querySelector(".error-text").classList.remove("active");
+            $submitBtn.disabled = !1;
+            $submitBtn.removeClass("disabled");
+        } else {
+            $("#text").css("border-color", "red");
+            e.target.parentElement.parentElement.querySelector(".error-text").classList.add("active");
+            $submitBtn.disabled = !0;
+            $submitBtn.addClass("disabled");
+        }
     });
 });
